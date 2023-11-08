5c5d65f755693d8573449441506ca3e185e7c72f
SkyzerFlyzer <skyzerflyzer@gmail.com>
2023-11-08 11:42:57 +0000

    Updated sitemap

--- SkyzerDev/sitemaps.py
+++ SkyzerDev/sitemaps.py
@@ -4,7 +4,8 @@ from django.shortcuts import reverse
 
 class StaticViewSitemap(Sitemap):
     def items(self):
-        return ['index', 'about', 'nitrado_server_guardian', 'robots']
+        return ['index', 'about', 'nitrado_server_guardian', 'robots', 'premium_features', 'commands',
+                'premium_commands', 'terms_of_service', 'privacy_policy', 'cookies', 'blog']
 
     def location(self, item):
         return reverse(item)
