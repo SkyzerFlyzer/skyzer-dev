1ed0b8de51a5d11a2acd42765f252e0ce5d52ab9
SkyzerFlyzer <skyzerflyzer@gmail.com>
2023-11-12 17:00:27 +0000

    Updated sitemap to force https

--- SkyzerDev/sitemaps.py
+++ SkyzerDev/sitemaps.py
@@ -3,6 +3,9 @@ from django.shortcuts import reverse
 
 
 class StaticViewSitemap(Sitemap):
+    protocol = 'https'
+    changefreq = "weekly"
+
     def items(self):
         return ['index', 'about', 'nitrado_server_guardian', 'robots', 'premium_features', 'commands',
                 'premium_commands', 'terms_of_service', 'privacy_policy', 'cookies', 'lotus:article-index']
